1e8dd31454b7651b86391055c5598dd6ec655a3f
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-09-01 23:36:08 +0330

    set up authentication system with clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
 	return (
 		<main>
-			<Button>dsfdsf</Button>
+			<UserButton afterSignOutUrl="/" />
 		</main>
 	);
 }
