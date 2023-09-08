b3f11a757deabcc05c2e0f031838c32fa914e94b
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-09-08 19:36:25 +0330

    add database schema, add prisma instance

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
 	return (
 		<main>
 			<UserButton afterSignOutUrl="/" />
+			<ModeToggle />
 		</main>
 	);
 }
