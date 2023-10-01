4965f9417e68ee9fa00a3176b07aedaa7b825709
Abolfazl Omidvar <a.omidvar@mohaymen.ir>
2023-10-01 14:16:39 +0330

    add navigation sidebar and tooltip component

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,26 @@
+'use client'
+import {Plus} from "lucide-react";
+import ActionTooltip from "@/components/action-tooltip";
+
+const NavigationAction = () => {
+    return (
+        <div>
+            <ActionTooltip
+                label='Add a server'
+                align='center'
+                side='right'>
+                <button className='group flex items-center'>
+                    <div
+                        className='flex items-center justify-center mx-3 h-[48px] w-[48px] rounded-[24px] hover:rounded-[16px] transition-all overflow-hidden bg-background dark:bg-neutral-700 group-hover:bg-emerald-400'>
+                        <Plus className='group-hover:text-zinc-100 transition text-emerald-400' size={25}/>
+                    </div>
+                </button>
+            </ActionTooltip>
+
+        </div>
+
+
+    );
+};
+
+export default NavigationAction
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,30 @@
-const NavigationSidebar = () => {
+import {currentProfile} from "@/lib/current-profile";
+import {redirect} from "next/navigation";
+import {prisma} from "@/lib/db";
+import NavigationAction from "@/components/navigation/navigation-action";
+
+const NavigationSidebar = async () => {
+    const profile = await currentProfile();
+
+    if (!profile) return redirect('/');
+
+    const server = await prisma.server.findMany({
+        where: {
+            Member: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    const x = 5
+
     return (
-        <div>
-            Navigation Sidebar
+        <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
+            <NavigationAction/>
         </div>
     );
 };
 
-export default  NavigationSidebar
+export default NavigationSidebar
