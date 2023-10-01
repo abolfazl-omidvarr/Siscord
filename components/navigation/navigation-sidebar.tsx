3f7a7e89a584e575bba73e89b55227bb02140ce4
Abolfazl Omidvar <a.omidvar@mohaymen.ir>
2023-10-01 14:23:17 +0330

    add scroll-area package

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,9 @@
 import {currentProfile} from "@/lib/current-profile";
+import {Separator} from "@/components/ui/separator";
+import NavigationAction from "@/components/navigation/navigation-action";
+
 import {redirect} from "next/navigation";
 import {prisma} from "@/lib/db";
-import NavigationAction from "@/components/navigation/navigation-action";
 
 const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -23,6 +25,8 @@ const NavigationSidebar = async () => {
     return (
         <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
             <NavigationAction/>
+            <Separator
+            className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto'/>
         </div>
     );
 };
